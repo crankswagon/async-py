1df1e6254c8e21ea83e064ad981db9506a2e56b7
Jay Jiang <48189996+crankswagon@users.noreply.github.com>
2021-09-27 20:54:26 +1000

    add multi session async

--- code/app.py
+++ code/app.py
@@ -1,34 +1,48 @@
-import json
 import aiohttp
 import asyncio
 import time
-import traceback
+
+
 
 async def hit_api(session, tar):
     async with session.get(tar['url']) as response:        
         await response.text()
         await asyncio.sleep(tar['lagsim']) #simulate some data processing lag here
-        return f"{tar['url']} responded at {response.headers.get('Date')}.... data processing finished at {time.strftime('%X')}"
+        print (f"{tar['metadata']}  |||| {tar['url']} responded at {response.headers.get('Date')}.... data processing finished at {time.strftime('%X')}")
+
+
+
+async def hit_api_group(sites):
+    """
+    this spawns a common session which is used to call the hit_api function
+    
+    """
+    async with aiohttp.ClientSession() as session:
+        return await asyncio.gather(*[hit_api(session, x) for x in sites]
+                                         ,return_exceptions=True
+                                         )
+
+        
 
 async def main(_targets):
     """
     adapted from this example: https://github.com/geeogi/async-python-lambda-template/tree/master
     aiohttp: https://docs.aiohttp.org/en/stable/
     asyncio: https://docs.python.org/3/library/asyncio.html
+
+    for each target_group, make its own task with accompaying sessions, so that the # of sessions is dictated by number of target groups in lambda event
         
     """
     print(f"started at {time.strftime('%X')}")
     
-    async with aiohttp.ClientSession() as session:
-        responses = await asyncio.gather(*[hit_api(session, x) for x in _targets]
-                                         ,return_exceptions=True
-                                         )
+    results= [asyncio.create_task(hit_api_group(target)) for target in _targets]
+    await asyncio.gather(*results)
 
-    print(responses)
+    print(results)
 
     print(f"finished at {time.strftime('%X')}")
 
 def lambda_handler(event, context):
-    targets=event["target_group"]
+    targets=event["target_groups"]
     asyncio.run(main(targets))
     
