af11852e427131914dd6ce04ef9134ad35210ba2
Jay Jiang <48189996+crankswagon@users.noreply.github.com>
2021-09-27 20:32:18 +1000

    more pythonic and more lamda-esque
    remove the error checking boiler plate for succinctness
    refactor to take a list of inputs from lambda event
    remove coroutine boiler plate (this aint Go)

--- code/app.py
+++ code/app.py
@@ -4,16 +4,13 @@ import asyncio
 import time
 import traceback
 
-
-async def hit_api(session, url, **kwargs):
-    async with session.get(url) as response:        
+async def hit_api(session, tar):
+    async with session.get(tar['url']) as response:        
         await response.text()
-        await asyncio.sleep(kwargs["_lagsim"]) #simulate some data processing lag here
-        return f"{url} responded at {response.headers.get('Date')}.... data processing finished at {time.strftime('%X')}"
+        await asyncio.sleep(tar['lagsim']) #simulate some data processing lag here
+        return f"{tar['url']} responded at {response.headers.get('Date')}.... data processing finished at {time.strftime('%X')}"
 
-
-
-async def main():
+async def main(_targets):
     """
     adapted from this example: https://github.com/geeogi/async-python-lambda-template/tree/master
     aiohttp: https://docs.aiohttp.org/en/stable/
@@ -21,28 +18,17 @@ async def main():
         
     """
     print(f"started at {time.strftime('%X')}")
-
+    
     async with aiohttp.ClientSession() as session:
-        coroutines = [
-            hit_api(session, 'https://google.com', _lagsim = 5),
-            hit_api(session, 'https://facebook.com', _lagsim = 10)
-        ]
-        results = await asyncio.gather(*coroutines, return_exceptions=True)
-    
-    err = None
-    for result, coro in zip(results, coroutines):
-        if isinstance(result, Exception):
-            err = result
-            print(f"{coro.__name__} failed:")
-            traceback.print_exception(type(err), err, err.__traceback__)
+        responses = await asyncio.gather(*[hit_api(session, x) for x in _targets]
+                                         ,return_exceptions=True
+                                         )
 
-    if err:
-        raise RuntimeError("derp something went wrong")
-    
-    print(results)
+    print(responses)
 
     print(f"finished at {time.strftime('%X')}")
 
 def lambda_handler(event, context):
-    asyncio.run(main())
+    targets=event["target_group"]
+    asyncio.run(main(targets))
     
